0bae6f6fdd23992fe78d508256271767b2f51c8a
Kaushik Kalyan Bojja <thequadheadedcoder@gmail.com>
2026-04-02 16:11:59 +0530

    Initial Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cultura',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Cultura'),
+      ),
+      body: Center(
+          child: const Text('Initial Setup'),
+        ),
+    );
+  }
+}
